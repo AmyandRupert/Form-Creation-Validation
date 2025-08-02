ccec5e8b4f585ca04a2e532d33936e55aa5185e3
AmyandRupert <amyleroux432@gmail.com>
2025-08-02 20:28:58 +0200

    Updated index.html style.css script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded"), (event) => {
   console.log("DOM fully loaded and parsed");}
-const form = document.getElementById("registrationForm");
+const form = document.getElementById("registration-form");
 const feedbackDiv = document.getElementById("form-feedback");
 form.addEventListener("submit"), function(event) {event.preventDefault();}
 const username = document.getElementById("username").value;
