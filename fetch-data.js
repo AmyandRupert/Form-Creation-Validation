f9da14d8076856c31c006f3ed474c60d9168e7fd
AmyandRupert <amyleroux432@gmail.com>
2025-08-03 21:03:33 +0200

    Added fetch-data.html fetch-data.css fetch-data.js

---
+++ fetch-data.js
@@ -0,0 +1,16 @@
+async function fetchUserData(params) {
+const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+const dataContainer = document.getElementById ('api-data');
+try {
+const response = await fetch(apiUrl);}
+const users= await response.json();
+dataContainer.innerHTML = '';
+const userList = document.createElement('ul');
+users.forEach(user =>) {
+const listItem = document.createElement('li');}
+catch (error) {
+    console.error(`Error fetching data: ${'Failed to load user data.'}`); 
+}
+document.addEventListener('DOMContentLoaded'), () => {
+    fetchUserData();
+}
